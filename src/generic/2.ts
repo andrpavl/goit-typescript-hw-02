d46f9a385ea56ede20c46f85546cbde7328dcd52
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-10-25 21:17:00 +0200

    hw is done

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -5,19 +5,34 @@
 */
 
 type AllType = {
-  name: string;
-  position: number;
-  color: string;
-  weight: number
+	name: string;
+	position: number;
+	color: string;
+	weight: number;
+};
+
+function compare(
+	top: Pick<AllType, "name" | "color">,
+	bottom: Pick<AllType, "position" | "weight">
+): AllType {
+	return {
+		name: top.name,
+		color: top.color,
+		position: bottom.position,
+		weight: bottom.weight,
+	};
 }
 
-function compare (top, bottom): AllType {
-  return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-  }
-}
+// function compare<T extends AllType, U extends AllType>(
+// 	top: Pick<T, keyof AllType>,
+// 	bottom: Pick<U, keyof AllType>
+// ): AllType {
+// 	return {
+// 		name: top.name,
+// 		color: top.color,
+// 		position: bottom.position,
+// 		weight: bottom.weight,
+// 	};
+// }
 
 export {};
